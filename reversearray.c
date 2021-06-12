35787e37597a5c85d929198f6a60b99216b868cf
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-06-12 20:25:32 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i, a;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+
+    for(i = 0; i < num  ; i++)
+        printf("%d ", *(arr +(num-1)- i));
+    return 0;
+}
 
