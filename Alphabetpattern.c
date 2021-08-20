8744b77ca8a020a1021877c5a3c3d619aa3acddb
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-08-20 21:54:17 +0530

    Update Alphabetpattern.c

--- Alphabetpattern.c
+++ Alphabetpattern.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main() {
+    int input,i;
+  scanf("%d",&input);
+  for(i=0;i<=input;i++)
+    {
+    if(i%2==0)
+      {
+      printf("%d is a even number",i);
+
+
+      }else{
+      printf("%d is a odd number",i);
+      }
+
+   
+    
+    return 0;
+}
